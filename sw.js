557e58f670cfdf60429d73e199314d4969b5038b
hunkoys <hunkyminik@gmail.com>
2026-07-20 11:44:19 +0800

    Playing

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'audio-app-v1';
+
+// The exact list of files to save offline
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/rain.wav'];
+
+// Install phase: Download and cache the files
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+// Fetch phase: Intercept network requests and serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
