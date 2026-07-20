1bcc3c071c67a23871596c2a37cceb0e5755d670
hunkoys <hunkyminik@gmail.com>
2026-07-20 15:24:44 +0800

    Restyled layout and added sound options

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'audio-app-v1';
 
 // The exact list of files to save offline
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/rain.mp3'];
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/rain.mp3', '/smooth-brown-noise.mp3'];
 
 // Install phase: Download and cache the files
 self.addEventListener('install', (event) => {
