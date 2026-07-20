43edaa2d0940bbc114d9d0a4c34f477cb1749441
hunkoys <hunkyminik@gmail.com>
2026-07-20 18:27:17 +0800

    New Sounds and cached on load

--- sw.js
+++ sw.js
@@ -1,11 +1,37 @@
 const CACHE_NAME = 'audio-app-v1';
 
 // The exact list of files to save offline
-const urlsToCache = ['/', '/index.html', '/manifest.json', '/rain.mp3', '/smooth-brown-noise.mp3'];
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/rain.aac',
+  '/brown.aac',
+  '/pink.aac',
+  '/smooth-pink.aac',
+  '/dense-pink.aac',
+];
 
 // Install phase: Download and cache the files
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+// Cleanup phase: Deletes the old caches so you don't waste storage
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
 });
 
 // Fetch phase: Intercept network requests and serve from cache
